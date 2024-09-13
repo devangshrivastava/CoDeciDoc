fd5396e0b290f8e2674b3f4c876187c00c9ac7bd
anurag161 <anuragkumar.bh@gmail.com>
2024-09-13 16:52:45 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,25 @@
+const { app, BrowserWindow, protocol } = require("electron");
+const url = require("url");
+const path = require("path");
+
+function createMainWindow() {
+  const mainWindow = new BrowserWindow({
+    title: "Electron",
+    width: 1000,
+    height: 600,
+    webPreferences: {
+      webSecurity: false
+    }
+  });
+
+  mainWindow.webContents.openDevTools();
+
+  const startUrl = url.format({
+    pathname: path.join(__dirname, "./app/build/index.html"),
+    protocol: "file",
+  });
+
+  mainWindow.loadURL("http://localhost:3000");
+}
+
+app.whenReady().then(createMainWindow);
